50b22d6ee51a4aa6143e287dd88183d755b21cc0
kalfritz <luizeliasceem@gmail.com>
2020-07-13 15:34:32 -0300

    config

--- src/server.js
+++ src/server.js
@@ -1,25 +1,27 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const path = require('path');
+require("dotenv").config();
 
-const socketio = require('socket.io');
-const http = require('http');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
 
-const routes = require('./routes');
+const socketio = require("socket.io");
+const http = require("http");
+
+const routes = require("./routes");
 
 const app = express();
 const server = http.Server(app);
 const io = socketio(server);
 
-mongoose.connect('mongodb://localhost:27017/aircnc', {
+mongoose.connect(process.env.DB_CONNECTION, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 const connectedUsers = {};
 
-io.on('connection', socket => {
+io.on("connection", (socket) => {
   const { user_id } = socket.handshake.query;
 
   connectedUsers[user_id] = socket.id;
@@ -34,7 +36,7 @@ app.use((req, res, next) => {
 
 app.use(cors());
 app.use(express.json());
-app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
 const port = 3333;
